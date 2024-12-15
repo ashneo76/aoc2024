50b81b3df4de22a3931abc5ea472bc3b8fd514ec
Ashish Shah <git@ashishshah.net>
2024-12-15 11:35:05 -0600

    documentation

--- clj/src/pz1.clj
+++ clj/src/pz1.clj
@@ -1,6 +1,8 @@
 (ns pz1
   [:require [clojure.test :as t]])
 
+;; -- problem 1
+;; util
 (defn parse-csv [raw]
   (let [rows (clojure.string/split raw #"\n")
         tuples (map #(clojure.string/split % #" +") rows)
@@ -9,10 +11,22 @@
         zipped (map vector left right)]
     zipped))
 
+;; util test
+(def test2 "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9")
+;; (parse-csv test2)
+;; (def parsed-rows (parse-csv (slurp "data/1.csv")))
+
+;; actual solution
 (defn pz1 [parsed-rows]
   (let [distances (map #(abs (- (first %) (second %))) parsed-rows)]
     (reduce + distances)))
 
+;; test
 (def test-data "3   4
 4   3
 2   5
@@ -22,14 +36,19 @@
 (def raw test-data)
 (t/is (= 11 (pz1 (parse-csv test-data))))
 
+;; apply to actual data
 (println (pz1 (parse-csv (slurp "data/1.csv"))))
 
+;; -- problem 2
+;; util
 (defn occurrences [itm lst]
   (count (filter #(= % itm) lst)))
+
+;; test occurrences
 (t/is (= 0 (occurrences 3 [1 2 5])))
 (t/is (= 2 (occurrences 3 [1 2 5 3 4 3])))
 
-;; (def parsed-rows (parse-csv (slurp "data/1.csv")))
+;; actual solution
 (defn pz2 [parsed-rows]
   (let [left (map first parsed-rows)
         right (map second parsed-rows)
@@ -38,13 +57,5 @@
     (reduce + freq)))
 (t/is (= 31 (pz2 (parse-csv test-data))))
 
+;; apply to actual data
 (println (pz2 (parse-csv (slurp "data/1.csv"))))
-
-
-(def test2 "7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9")
-(parse-csv test2)
